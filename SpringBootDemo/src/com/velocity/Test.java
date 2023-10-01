23f388da44481969a4f065f25d909dfcf29dd982
unknown <dipikazade25@gmail.com>
2023-10-01 21:21:57 +0530

    added the array list

---
+++ SpringBootDemo/src/com/velocity/Test.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+		List list=new ArrayList();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		System.out.println(list);
+	}
+
+}
