32ab13b4fdf1274a6205cdc2f4254833f52d1b8e
unknown <dipikazade25@gmail.com>
2023-10-01 22:52:31 +0530

    changes in Test.java and used generics

--- SpringBootDemo/src/com/velocity/Test.java
+++ SpringBootDemo/src/com/velocity/Test.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Test {
 	public static void main(String[] args) {
-		List list=new ArrayList();
-		list.add(10);
-		list.add(20);
-		list.add(30);
+		List<String> list=new ArrayList<String>();
+		list.add("10");
+		list.add("20");
+		list.add("30");
 		System.out.println(list);
 	}
 
