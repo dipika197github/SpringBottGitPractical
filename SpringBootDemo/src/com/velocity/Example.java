39cfa23c9b759157554b99c9e06a73dae74cdb44
unknown <dipikazade25@gmail.com>
2023-10-01 23:09:37 +0530

    changes in example class

--- SpringBootDemo/src/com/velocity/Example.java
+++ SpringBootDemo/src/com/velocity/Example.java
@@ -3,7 +3,7 @@ package com.velocity;
 public class Example {
 public static void main(String[] args) {
 	for(int j=1;j<=1;j++) {
-		System.out.println(j);
+		System.out.println("java code is>>>" +j);
 	}
 }
 }
